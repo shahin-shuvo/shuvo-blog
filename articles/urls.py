e7262bbf91d64eb10e86284d837e2f644b662a56
shahin-shuvo <shahinshuvo96@gmail.com>
2020-04-08 04:09:06 +0600

    Final Version

--- articles/urls.py
+++ articles/urls.py
@@ -5,10 +5,7 @@ from . import views
 app_name = 'articles'
 urlpatterns = [
 
-    # /profileView
-    path('<token>/', views.ProfileListView.as_view()),
-    
-    path('comment/create/', views.CommentCreateView.as_view()),
+    path('comment/create/', views.CommentCreateView.as_view())
 
     path('comment/<postid>/', views.CommentRetriveView.as_view()),
 
